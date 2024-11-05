de7578338af76d5c3afe11628ba616a060e88601
Kmbasavaraj <basavakm2023@gmail.com>
2024-11-05 10:07:36 +0530

    Project Initiated pom.xml file added

---
+++ src/test/java/com/jsp/pharma/PharmaassistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jsp.pharma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PharmaassistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
